db6b29739bb3fb04d89755995b472289b23bb739
Adriani277 <af277@hotmail.com>
2020-04-12 15:11:50 +0100

    Changed tests to property based

--- src/test/scala/functionalscalaws/ConsoleSpec.scala
+++ src/test/scala/functionalscalaws/ConsoleSpec.scala
@@ -5,39 +5,46 @@ import cats.effect.concurrent.Ref
 import cats.effect.IO
 import cats.implicits._
 import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import org.scalacheck.Gen
 
-final class ConsoleSpec extends AnyFunSpec with Matchers {
+final class ConsoleSpec extends AnyFunSpec with Matchers with ScalaCheckPropertyChecks {
   describe("Console") {
     it("putStrLn puts a string into the buffer") {
-      Ref
-        .of[IO, List[String]](List.empty)
-        .flatMap { ref =>
-          val underTest = testConsole(ref)
+      forAll(Gen.alphaNumStr) { str =>
+        Ref
+          .of[IO, List[String]](List.empty)
+          .flatMap { ref =>
+            val underTest = testConsole(ref)
 
-          underTest.putStrLn("Hi") *>
-            ref.get.map { b =>
-              b.head shouldBe "Hi"
-            }
-        }
-        .unsafeRunSync()
+            underTest.putStrLn(str) *>
+              ref.get.map { b =>
+                b.head shouldBe str
+              }
+          }
+          .unsafeRunSync()
+      }
     }
 
     it("getStrLn gets a string from the buffer") {
-      Ref
-        .of[IO, List[String]](List("World"))
-        .flatMap { ref =>
-          val underTest = testConsole(ref)
+      forAll(Gen.nonEmptyListOf(Gen.alphaNumStr)) { buffer =>
+        Ref
+          .of[IO, List[String]](buffer)
+          .flatMap { ref =>
+            val underTest = testConsole(ref)
 
-          underTest.getStrLn().map { result =>
-            result shouldBe "World"
+            underTest.getStrLn().map { result =>
+              result shouldBe buffer.mkString
+            }
           }
-        }
-        .unsafeRunSync()
+          .unsafeRunSync()
+      }
     }
   }
 
-  private def testConsole(buffer: Ref[IO, List[String]]) = new functionalscalaws.algebras.Console[IO] {
-    def getStrLn(): IO[String]        = buffer.get.map(_.mkString)
-    def putStrLn(s: String): IO[Unit] = buffer.set(List(s))
-  }
+  private def testConsole(buffer: Ref[IO, List[String]]) =
+    new functionalscalaws.algebras.Console[IO] {
+      def getStrLn(): IO[String]        = buffer.get.map(_.mkString)
+      def putStrLn(s: String): IO[Unit] = buffer.set(List(s))
+    }
 }
--- src/test/scala/functionalscalaws/http/HelloRoutesSpec.scala
+++ src/test/scala/functionalscalaws/http/HelloRoutesSpec.scala
@@ -1,24 +1,28 @@
 package functionalscalaws.http
 
+import cats.effect.IO
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
-import cats.effect.IO
+import org.http4s._
 import org.http4s.implicits._
-import org.http4s.Request
-import org.http4s.Method
-import org.http4s.Status
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import org.scalacheck.Gen
+import org.http4s.Uri
 
-final class HelloRoutesSpec extends AnyFunSpec with Matchers {
+final class HelloRoutesSpec extends AnyFunSpec with Matchers with ScalaCheckDrivenPropertyChecks {
   describe("HelloRoutes") {
-    it("returns Ok") {
-      val response = HelloRoutes[IO]().helloWorldService.orNotFound
-        .run(
-          Request(method = Method.GET, uri = uri"/hello/Adriani")
-        )
-        .unsafeRunSync
+    it("returns Ok response for /hello/<name> endpoint") {
+      forAll(Gen.alphaStr) { str =>
+        val uri = Uri.unsafeFromString(s"/hello/$str")
+        val response = HelloRoutes[IO]().helloWorldService.orNotFound
+          .run(
+            Request(method = Method.GET, uri = uri)
+          )
+          .unsafeRunSync
 
-      response.status shouldBe Status.Ok
-      response.as[String].unsafeRunSync shouldBe "Hello, Adriani"
+        response.status shouldBe Status.Ok
+        response.as[String].unsafeRunSync shouldBe s"Hello, $str"
+      }
     }
   }
 }
