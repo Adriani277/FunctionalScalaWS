d011e0013b766c57bca43b4aa2d5789dcbe0ca7d
Adriani Furtado <af277@hotmail.com>
2020-11-11 21:41:39 +0000

    Added create tests

--- src/test/scala/functionalscalaws/persistence/PersistenceSpec.scala
+++ src/test/scala/functionalscalaws/persistence/PersistenceSpec.scala
@@ -12,22 +12,41 @@ object PersistenceSpec extends DefaultRunnableSpec {
 
   val usersGen = Gen.vectorOfBounded(1, 10)(userGen)
 
-  def spec = suite("inMemory") {
-    suite("get") {
-      testM("returns error when user does not exist") {
-        checkM(Gen.anyInt) { id =>
-          assertM(get[User](id).run)(
-            fails(isSubtype[RuntimeException](anything))
-          ).provideLayer(inMemory(Vector.empty))
-        }
-      }
+  def spec = suite("inMemory")(getTests, createTests)
 
-      testM("returns a user when id exists") {
-        checkM(usersGen) { users =>
-          assertM(get[User](users.head.id))(
-            equalTo(users.head)
-          ).provideLayer(inMemory(users))
-        }
+  val getTests = suite("get") {
+    testM("returns error when user does not exist") {
+      checkM(Gen.anyInt) { id =>
+        assertM(get[User](id).run)(
+          fails(isSubtype[RuntimeException](anything))
+        ).provideLayer(inMemory(Vector.empty))
+      }
+    }
+
+    testM("returns a user when id exists") {
+      checkM(usersGen) { users =>
+        assertM(get[User](users.head.id))(
+          equalTo(users.head)
+        ).provideLayer(inMemory(users))
+      }
+    }
+  }
+
+  val createTests = suite("create") {
+    testM("creates a new user") {
+      checkM(userGen) { user =>
+        assertM(create[User](user))(
+          equalTo(user)
+        ).provideLayer(inMemory(Vector.empty))
+      }
+    }
+
+    testM("updates existing user") {
+      checkM(userGen, userGen) { (user1, user2) =>
+        val newUser = user2.copy(user1.id)
+        assertM(create[User](user1) *> create[User](newUser))(
+          equalTo(newUser)
+        ).provideLayer(inMemory(Vector.empty))
       }
     }
   }
